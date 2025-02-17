3cdb59bcbaffd89a8fcf34bdc7d060cff033370d
PhamQuangTrong <22130299@st.hcmuaf.edu.vn>
2025-02-17 20:49:21 +0700

    FE mixer-shops project initiation

---
+++ src/test/java/mixer_shops/mixer/MixerApplicationTests.java
@@ -0,0 +1,13 @@
+package mixer_shops.mixer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
